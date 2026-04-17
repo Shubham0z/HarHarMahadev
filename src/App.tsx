010bce8db314a7d87356a416d3f10990459b32c4
Shubham0z <shubham2435singh@gmail.com>
2026-04-17 18:53:43 +0530

    Add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,24 @@ import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import DashboardPage from './pages/DashboardPage';
 import AnalyzePage from './pages/AnalyzePage';
+import RoutesPage from "./pages/RoutePage";
+import RiskPage from './pages/RiskPage';
+import SuppliersPage from './pages/SuppliersPage';
+import CostPage from './pages/CostPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout><HomePage /></Layout>} />
-        <Route path="/dashboard" element={<Layout><DashboardPage /></Layout>} />
-        <Route path="/analyze" element={<Layout><AnalyzePage /></Layout>} />
+        <Route path="/"           element={<Layout><HomePage /></Layout>}      />
+        <Route path="/dashboard"  element={<Layout><DashboardPage /></Layout>} />
+        <Route path="/analyze"    element={<Layout><AnalyzePage /></Layout>}   />
+        <Route path="/routes"     element={<Layout><RoutesPage /></Layout>}    />
+        <Route path="/risk"       element={<Layout><RiskPage /></Layout>}      />
+        <Route path="/suppliers"  element={<Layout><SuppliersPage /></Layout>} />
+        <Route path="/costs"      element={<Layout><CostPage /></Layout>}      />
         {/* Catch all → home */}
-        <Route path="*" element={<Layout><HomePage /></Layout>} />
+        <Route path="*"           element={<Layout><HomePage /></Layout>}      />
       </Routes>
     </BrowserRouter>
   );
