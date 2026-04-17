ac78ec023667061a8652d2943057e1a3678d95cf
Shubham0z <shubham2435singh@gmail.com>
2026-04-17 19:35:41 +0530

    Add ScenariosPage route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-// src/App.tsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import DashboardPage from './pages/DashboardPage';
 import AnalyzePage from './pages/AnalyzePage';
-import RoutesPage from "./pages/RoutePage";
+import RoutesPage from './pages/RoutePage';
 import RiskPage from './pages/RiskPage';
 import SuppliersPage from './pages/SuppliersPage';
 import CostPage from './pages/CostPage';
+import ScenariosPage from './pages/ScenariosPage';
 
 export default function App() {
   return (
@@ -20,6 +20,7 @@ export default function App() {
         <Route path="/risk"       element={<Layout><RiskPage /></Layout>}      />
         <Route path="/suppliers"  element={<Layout><SuppliersPage /></Layout>} />
         <Route path="/costs"      element={<Layout><CostPage /></Layout>}      />
+        <Route path="/scenarios"  element={<Layout><ScenariosPage /></Layout>} />
         {/* Catch all → home */}
         <Route path="*"           element={<Layout><HomePage /></Layout>}      />
       </Routes>
